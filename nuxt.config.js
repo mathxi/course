dc6a7586fdd2598904f8ec21c8d55fb9b8590226
Mathieu Janio <mathieu.janio@epsi.fr>
2020-07-24 21:00:06 +0200

    test deployement

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  server: {
+    port:80,
+    host: '0.0.0.0'
+  },
   /*
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
